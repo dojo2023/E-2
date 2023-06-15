b0c28465151f95cc1c5e4e74b6a20d5a4ee136c1
dojo2023-murakami <130331042+dojo2023-murakami@users.noreply.github.com>
2023-06-15 16:30:40 +0900

    ガチャページ加筆

--- src/src/servlet/GachaServlet.java
+++ src/src/servlet/GachaServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Random;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,20 +11,48 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.GachaDAO;
+import model.Gacha;
+
+/**
+ * Servlet implementation class GachaServlet
+ */
+@WebServlet("/GachaServlet")
+public class GachaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 
 	/**
-	 * Servlet implementation class GachaServlet
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	@WebServlet("/GachaServlet")
-	public class GachaServlet extends HttpServlet {
-		private static final long serialVersionUID = 1L;
-
-		/**
-		 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-		 */
-		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			// フォワード
-			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/gacha.jsp");
-			dispatcher.forward(request,response);
-		}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// フォワード
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/gacha.jsp");
+				dispatcher.forward(request,response);
 	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		GachaDAO dao = new GachaDAO();
+
+		// asigaruselect()のテスト
+		Random random = new Random();
+				List<Gacha> gachaList = dao.samuraiselect();
+
+					Gacha asigaru = gachaList.get(random.nextInt(5));
+					String asi = asigaru.getGachaname();
+					String asiimg = asigaru.getGachapath();
+
+						System.out.println("足軽ガチャ"+asigaru.getGachaname());
+
+						// ガチャ結果をリクエストスコープに格納する
+						request.setAttribute("asigaru", asi);
+						request.setAttribute("asiimg", asiimg);
+						//ガチャ結果ページにフォワードする
+						RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/gatyaresult.jsp");
+						dispatcher.forward(request, response);
+
+	}
+
+}
