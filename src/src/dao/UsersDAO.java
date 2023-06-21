2d5e332f2c007ccddf3bb03d1b81ccce9b44db06
dojo2023-hidakamiyuna <hidakamiyunaplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-21 18:03:15 +0900

    Update UsersDAO.java

--- src/src/dao/UsersDAO.java
+++ src/src/dao/UsersDAO.java
@@ -1,5 +1,113 @@
 package dao;
 
-public class UsersDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-}
+import model.Users;
+
+public class UsersDAO {
+	// ログインできるならtrueを返す
+		public boolean isLoginOK(Users users) {
+			Connection conn = null;
+			boolean loginResult = false;
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/db/GardenDB", "sa", "password");
+
+				// SELECT文を準備する
+				String sql = "select count(*) from USER where STAFF_ID = ? and PASSWORD = ?";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				pStmt.setString(1,users.getStaff_id());
+				pStmt.setString(2,users.getPassword());
+
+
+				// SELECT文を実行し、結果表を取得する
+				ResultSet rs = pStmt.executeQuery();
+
+				// ユーザーIDとパスワードが一致するユーザーがいたかどうかをチェックする
+				rs.next();
+				if (rs.getInt("count(*)") == 1) {
+					loginResult = true;
+				}
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+				loginResult = false;
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				loginResult = false;
+			}
+
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+						loginResult = false;
+					}
+				}
+			}
+			// 結果を返す
+			return loginResult;
+		}
+    //管理者を確認する
+    public String adceak(String id) {
+	    Connection conn = null;
+		String role= "";
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+					conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/db/GardenDB", "sa", "password");
+
+				// SQL文を準備する
+				 String sql = "select ROLE from USER where STAFF_ID = ?";
+				 	PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる
+					 pStmt.setString(1, id);
+
+				// SQL文を実行し、結果表を取得する
+					ResultSet rs = pStmt.executeQuery();
+					rs.next();
+					role = rs.getString("ROLE");
+
+
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+					}
+					catch (ClassNotFoundException e) {
+						e.printStackTrace();
+					}
+					finally {
+						// データベースを切断
+						if (conn != null) {
+							try {
+								conn.close();
+							}
+							catch (SQLException e) {
+								e.printStackTrace();
+							}
+						}
+					}
+
+					// 結果を返す
+					return role;
+				}
+	}
+
