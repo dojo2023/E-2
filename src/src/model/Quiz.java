3c82e1e5e767d5f79991cfabf989b1f9d1700286
dojo2023-uchidayu <130334831+dojo2023-uchidayu@users.noreply.github.com>
2023-06-21 18:03:11 +0900

    クイズ機能

--- src/src/model/Quiz.java
+++ src/src/model/Quiz.java
@@ -1,5 +1,37 @@
 package model;
+import java.io.Serializable;
 
-public class Quiz {
+public class Quiz implements Serializable{
+	private int quiz_id;
+	private String quiz;
+	private String quiz_ex;
+
+	public Quiz() {
+		super();
+	}
+
+	public int getQuiz_Id() {
+		return quiz_id;
+	}
+
+	public void setQuiz_Id(int quiz_id) {
+		this.quiz_id = quiz_id;
+	}
+
+	public String getQuiz() {
+		return quiz;
+	}
+
+	public void setQuiz(String quiz) {
+		this.quiz = quiz;
+	}
+
+	public String getQuiz_Ex() {
+		return quiz_ex;
+	}
+
+	public void setQuiz_Ex(String quiz_ex) {
+		this.quiz_ex = quiz_ex;
+	}
 
 }
