6ff5333bc605f12140e7e7792fef48d0877bbb06
dojo2023-hidakamiyuna <hidakamiyunaplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-21 18:02:55 +0900

    Update login.js

--- src/WebContent/js/login.js
+++ src/WebContent/js/login.js
@@ -1,3 +1,28 @@
 /**
- * 
+ *
  */
+//現在の時間を取得する
+const hour = new Date().getHours();
+
+//朝4時〜10時59分まで
+if(hour >= 4 && hour < 11){
+document.getElementById('greeting').textContent ="おはようございます！";
+//昼１１時〜1６時59分まで
+}else if(hour >=11 && hour <17){
+document.getElementById('greeting').textContent ="こんにちは！";
+//夜18時〜3時59分まで
+}else{
+document.getElementById('greeting').textContent ="こんばんは！";
+}
+// ログイン時の処理
+var formObj = document.getElementById('login_form');
+let staff_id = document.getElementById('login_staff_id_id');
+let password =document.getElementById('login_password');
+var errorMessageObj = document.getElementById('error_message');
+formObj.onsubmit = function() {
+  if (!staff_id.value || !password.value) {
+    errorMessageObj.textContent = '※IDとパスワードを入力してください!';
+    return false;
+  }
+  errorMessageObj.textContent = null;
+};
