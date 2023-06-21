174797d700203edfe9ef102910fef9471038af59
dojo2023-hidakamiyuna <hidakamiyunaplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-21 18:03:08 +0900

    Update MypageServlet.java

--- src/src/servlet/MypageServlet.java
+++ src/src/servlet/MypageServlet.java
@@ -1,5 +1,53 @@
 package servlet;
 
-public class MypageServlet {
+import java.io.IOException;
 
-}
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.MypageDAO;
+import model.Mypage;
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/MypageServlet")
+public class MypageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("staff_id") == null) {
+			response.sendRedirect("/kumano_onigiri/LoginServlet");
+			return;
+		}
+
+		// マイページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/mypage.jsp");
+		dispatcher.forward(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String staff_id = request.getParameter("STAFF_ID");
+		String name = request.getParameter("NAME");
+		String role = request.getParameter("ROLE");
+		String mark_id = request.getParameter("MARK_ID");
+		String gatya_id = request.getParameter("GATYA_ID");
+		String task_thread = request.getParameter("TASK_THREAD");
+
+
+
+			// マイページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/mypage.jsp");
+			dispatcher.forward(request, response);
+		}
+    }
