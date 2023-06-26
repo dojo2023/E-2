728f01f5dc27ab98e9145545282d23ca67b2f5b4
dojo2023-sadanaga <130334358+dojo2023-sadanaga@users.noreply.github.com>
2023-06-26 15:16:07 +0900

    追加

--- src/src/model/Working.java
+++ src/src/model/Working.java
@@ -1,5 +1,80 @@
 package model;
+import java.io.Serializable;
 
-public class Working {
+public class Working implements Serializable {
+	private String WORK_ID;
+	private String WORK_START;
+	private String WORK_END;
+	private String WORK_STYLE;
+	private String STAFF_ID;
+	private String WORK_DATE;
 
+	public Working(String WORK_ID, String WORK_START, String WORK_END, String WORK_STYLE
+			,String STAFF_ID,String WORK_DATE){
+		super();
+		this.WORK_ID = WORK_ID;
+		this.WORK_START = WORK_START;
+		this.WORK_END = WORK_END;
+		this.WORK_STYLE = WORK_STYLE;
+		this.STAFF_ID = STAFF_ID;
+		this.WORK_DATE = WORK_DATE;
+	}
+
+	public Working() {
+		super();
+		this.WORK_ID = "";
+		this.WORK_START = "";
+		this.WORK_END = "";
+		this.WORK_STYLE = "";
+		this.STAFF_ID = "";
+		this.WORK_DATE = "";
+	}
+
+	public String getWORK_ID() {
+		return WORK_ID;
+	}
+
+	public void setWORK_ID(String WORK_ID) {
+		this.WORK_ID = WORK_ID;
+	}
+
+	public String getWORK_START() {
+		return WORK_START;
+	}
+
+	public void setCompany(String WORK_START) {
+		this.WORK_START = WORK_START;
+	}
+
+	public String getWORK_END() {
+		return WORK_END;
+	}
+
+	public void setDepartment(String WORK_END) {
+		this.WORK_END = WORK_END;
+	}
+
+	public String getWORK_STYLE() {
+		return WORK_STYLE;
+	}
+
+	public void setWORK_STYLE(String WORK_STYLE) {
+		this.WORK_STYLE = WORK_STYLE;
+	}
+
+	public String getSTAFF_ID() {
+		return STAFF_ID;
+	}
+
+	public void setSTAFF_ID(String STAFF_ID) {
+		this.STAFF_ID = STAFF_ID;
+	}
+
+	public String getWORK_DATE() {
+		return WORK_DATE;
+	}
+
+	public void setWORK_DATE(String WORK_DATE) {
+		this.WORK_DATE = WORK_DATE;
+	}
 }
--- src/src/servlet/WorkingServlet.java
+++ src/src/servlet/WorkingServlet.java
@@ -1,5 +1,70 @@
 package servlet;
 
-public class WorkingServlet {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.WorkingDAO;
+import model.Working;
+
+@WebServlet("/WorkingServlet")
+public class WorkingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		//ログインIDを取得
+    	HttpSession session = request.getSession();
+    	int staff_id = (int)session.getAttribute("staff_id");
+
+        // リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String work_id = request.getParameter("WORK_ID");
+		String work_start = request.getParameter("WORK_START");
+		String work_end = request.getParameter("WORK_END");
+		String work_style = request.getParameter("WORK_STYLE");
+		String work_date = request.getParameter("WORK_DATE");
+
+		// 検索処理を行う
+		WorkingDAO bDao = new WorkingDAO();
+		List<Working> cardList = bDao.select(new Working(work_date,work_start,work_end,work_style,staff_id,work_date));
+
+		// 検索結果をリクエストスコープに格納する
+		request.setAttribute("cardList", cardList);
+
+		// 勤怠ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/working.jsp");
+		dispatcher.forward(request, response);
+	    }
+
+        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    	//ログインIDを取得
+        HttpSession session = request.getSession();
+        String staff_id = (String)session.getAttribute("staff_id");
+
+            // リクエストパラメータを取得する
+    	request.setCharacterEncoding("UTF-8");
+    	String work_id = request.getParameter("WORK_ID");
+    	String work_start = request.getParameter("WORK_START");
+    	String work_end = request.getParameter("WORK_END");
+    	String work_style = request.getParameter("WORK_STYLE");
+    	String work_date = request.getParameter("WORK_DATE");
+
+		// 登録処理を行う
+     	WorkingDAO bDao = new WorkingDAO();
+	    if (bDao.insert(work_start,work_end,work_style,staff_id,work_date)) {
+
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/working.jsp");
+		dispatcher.forward(request, response);
+		}
+	}
 }
