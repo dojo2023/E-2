5258df8598293382a7911114682ebbd5c6d64256
dojo2023-watanabe <130334527+dojo2023-watanabe@users.noreply.github.com>
2023-06-27 14:52:16 +0900

    修正

--- src/src/servlet/StudyServlet.java
+++ src/src/servlet/StudyServlet.java
@@ -8,6 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.Back_groundDAO;
 
 
 /**
@@ -20,6 +23,17 @@ public class StudyServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		//ログインIDを取得
+		HttpSession session = request.getSession();
+		String staff_id = (String)session.getAttribute("staff_id");
+
+		Back_groundDAO bgdao = new Back_groundDAO();
+		bgdao.connect();
+		String bgid = bgdao.select(staff_id);//numのところにstaff_idを入れる
+		bgdao.disconnect();
+		request.setAttribute("bgid", bgid);
+
 		// フォワード
 				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/study.jsp");
 				dispatcher.forward(request,response);
